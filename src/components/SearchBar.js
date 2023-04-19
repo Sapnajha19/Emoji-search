acb5d44dc51820638ca2bb078d147fc0c9bde219
Sapna Jha <sapnajha1919@gmail.com>
2023-04-19 14:58:07 +0530

    Add SearchBar component and its style file

---
+++ src/components/SearchBar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./SearchBar.css"
+export default function SearchBar() {
+    
+  return (
+    
+      <input type="text" placeholder="Search emoji" style={{width:"1050px",height:"70px", background:"#302C66",color:"#ffffff",border:"none",borderRadius:"15px",outline:"none",fontSize:"24px",fontWeight:"400",fontStyle:"normal",fontFamily:"Basis Grotesque Pro",textIndent:"25px"}}/>
+    
+  )
+}
