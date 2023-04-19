95cdc0f6c93f15a37fc72ad111eb8029e0928707
Sapna Jha <sapnajha1919@gmail.com>
2023-04-19 14:55:56 +0530

    Add Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    
+      <h1 style={{color:"white",display:"flex",justifyContent:"center",position:"relative", top:"50px"}}>Emoji Search</h1>
+    
+  )
+}
